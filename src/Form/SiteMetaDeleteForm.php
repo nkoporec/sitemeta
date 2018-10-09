2908132c931c1c089c8846da211a9d62ff040005
Nejc <nejc.koporec@agiledrop.com>
2018-10-09 15:10:27 +0200

    Initial commit

---
+++ src/Form/SiteMetaDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\sitemeta\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Site meta entities.
+ *
+ * @ingroup sitemeta
+ */
+class SiteMetaDeleteForm extends ContentEntityDeleteForm {
+
+
+}
